9308547ec2e5d386cd7c6f7da28b850c1fd7a8af
Romel Palomino <romel.palomino@ARFVFGG2SVQ05P.local>
2022-05-07 08:29:32 -0300

    GS001 - Init Project

---
+++ GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Romel Palomino on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
